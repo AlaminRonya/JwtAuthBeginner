37e105b244b6aeec033a1656c2a286493e94cf90
AlaminRonya <hprony12@gmail.com>
2022-12-31 22:42:13 +0600

    Jwt Auth basic

---
+++ src/main/java/com/alamin/jwttokenimplbasicauth/JwtTokenImplBasicAuthApplication.java
@@ -0,0 +1,13 @@
+package com.alamin.jwttokenimplbasicauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTokenImplBasicAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtTokenImplBasicAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alamin/jwttokenimplbasicauth/JwtTokenImplBasicAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alamin.jwttokenimplbasicauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTokenImplBasicAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
